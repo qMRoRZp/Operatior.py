63e15c0da656c28de422b5c55af9e75b0e321919
Alireza Razavi <114526330+qMRoRZp@users.noreply.github.com>
2023-02-11 15:02:31 +0330

    Add files via upload

---
+++ Operatior.py
@@ -0,0 +1,7 @@
+num1 = 5
+num1 + 5 #num1 change to 10
+num1 / 2 #10 divided by 2 [	Division]
+num1 * 20 #times 20
+num1 // 4 # divided 4 [Floor division]
+num2 = num1 # put num1 to num2
+del num1 # delete num1
